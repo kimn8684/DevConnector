aeb85a7f8caa0f0c7df08d4d6e46f92c2297dc6c
Nuri Kim <kimn8684@gmail.com>
2019-03-15 02:24:27 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profiles = require('./routes/api/profiles');
+
+//Connect to db
+mongoose
+    .connect(db)
+    .then( () => console.log('mongoDB connected'))
+    .catch( err => console.log(err));
+    
+//Let's write our first route
+app.get('/', (req, res) => res.send('Hello!'));
+
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profiles', profiles);
+
+const port = process.env.PORT || 5200;
+app.listen(port, () => console.log(`Server running on port ${port}`));
