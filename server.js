34412de03ff552c18a1884a80466d681748b7f0e
Nuri Kim <kimn8684@gmail.com>
2019-04-19 01:47:25 -0700

    Heroku preparation

--- server.js
+++ server.js
@@ -31,5 +31,14 @@ app.use('/api/users', users);
 // app.use('/api/posts', posts);
 // app.use('/api/profiles', profiles);
 
+if (process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 6009;
 app.listen(port, () => console.log(`Server running on port ${port}`));
